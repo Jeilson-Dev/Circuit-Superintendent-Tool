9b49fa50cc4fcf5abee54758acb28609572990e6
Jeilson Araujo <jeilsonaraujo@icloud.com>
2024-05-10 08:41:34 -0300

    feat: setup project

---
+++ android/app/src/main/kotlin/com/example/circuit_superintendent_tool/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.circuit_superintendent_tool
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CSTApp());
+}
+
+class CSTApp extends StatefulWidget {
+  const CSTApp({super.key});
+
+  @override
+  State<CSTApp> createState() => _CSTAppState();
+}
+
+class _CSTAppState extends State<CSTApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
